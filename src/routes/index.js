5d23549e33d49062382a92e13bc619789bf6b42f
andytryn <andi7858@students.amikom.ac.id>
2022-05-20 05:32:02 +0200

    Modified: index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', header: 'Express - 👨‍💻 🤲🏻' });
 });
 
 module.exports = router;
